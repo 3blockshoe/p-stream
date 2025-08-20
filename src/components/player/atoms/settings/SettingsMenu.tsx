b21ab4cabe2d106fc2ef5949ebbee36ddd7f0410
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-08-19 22:32:40 -0600

    Update SettingsMenu.tsx

--- src/components/player/atoms/settings/SettingsMenu.tsx
+++ src/components/player/atoms/settings/SettingsMenu.tsx
@@ -86,7 +86,7 @@ export function SettingsMenu({ id }: { id: string }) {
         >
           {t("player.menus.settings.sourceItem")}
           <span className="text-type-secondary text-sm">
-            {sourceName} | {embedName}
+            {sourceName} {embedName && `| ${embedName}`}
           </span>
         </Menu.ChevronLink>
         <Menu.ChevronLink
